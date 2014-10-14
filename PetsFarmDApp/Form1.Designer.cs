8fcc12424f52274abe40f83fef9c39699a65493b
artemoff85 <artol85@yandex.ru>
2014-10-14 16:34:45 +0700

    c2
    c2

--- PetsFarmDApp/Form1.Designer.cs
+++ PetsFarmDApp/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(109, 95);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,21 +47,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(211, 105);
+            this.label1.Location = new System.Drawing.Point(114, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "_____";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 321);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 195);
+            this.ClientSize = new System.Drawing.Size(520, 387);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Pets farm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- PetsFarmDApp/Form1.cs
+++ PetsFarmDApp/Form1.cs
@@ -14,37 +14,42 @@ namespace PetsFarm
     public partial class Form1 : Form
     {
         cPet MyPet;
+        Pen aPen;
 
         public Form1()
         {
             InitializeComponent();
+            aPen = new Pen(Color.Red, 2);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Random rnd = new Random();
-            //int aPetType = rnd.Next(1, 2);
             MyPet = null;
             switch (new Random().Next(1, 3))
             {
                 case 1:
-                    //Console.WriteLine("Case 1");
                     MyPet = new cCat("Murka");
                     break;
                 case 2:
-                    //Console.WriteLine("Case 2");
                     MyPet = new cDog("Zhuchka");
                     break;
                 /*
                 default:
-                    //Console.WriteLine("Default case");
-                    MyPet = new cCat("Murka");
                     break;*/
             }
             if (MyPet != null)
             {
                 label1.Text = MyPet.doVoice();
             }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //pictureBox1.CreateGraphics().DrawRectangle(aPen, 5, 5, 100, 100);
+            //using (Pen pen = new Pen(Color.Red, 2))
+            //{
+            e.Graphics.DrawRectangle(aPen, 5, 5, 100, 100);
+            //}
         }
     }
 }
