d038aabea113179b074fe26cd132243088967254
artemoff85 <artol85@yandex.ru>
2014-10-15 09:18:59 +0700

    c6
    c6

--- PetsFarmDApp/Form1.cs
+++ PetsFarmDApp/Form1.cs
@@ -18,16 +18,74 @@ namespace PetsFarm
         Brush aBrush;
         Font aFont;
 
+        int[,] aFarm;
+        int iPetSCat = 1;
+        int iPetSDog = 2;
+        int iPetCat = 3;
+        int iPetDog = 4;
+
+
+        private int[,] InitFarm(int _cols, int _rows, int _petsCount)
+        {
+            //fill farm by empty cells
+            int[,] _aFarm = new int[_cols, _rows];
+            for (int c = 0; c < _cols; c++)
+                for (int r = 0; r < _rows; r++)
+                    _aFarm[c, r] = 0;
+            //add pets to farm
+            Random rPet = new Random();
+            Random rCoord = new Random();
+            int[] rCols = new int[_petsCount+1];
+            for (int i = 0; i < rCols.Length; i++)
+                rCols[i] = rCoord.Next(0, _cols);
+
+            int[] rRows = new int[_petsCount+1];
+            for (int i = 0; i < rRows.Length; i++)
+                rRows[i] = rCoord.Next(0, _rows);
+
+            for (int i = 0; i < _petsCount + 1; i++)
+                _aFarm[rCols[i], rRows[i]] = rPet.Next(1, 3);
+
+            return _aFarm;
+        }
+
+        private void RenderFarm(int[,] _aFarm, Graphics gCanvas)
+        {
+            int _size = 15;
+            int _cols = _aFarm.GetLength(0);
+            int _rows = _aFarm.GetLength(1);
+            int iFarmX = 0;
+            int iFarmY = 0;
+            //Text = _cols.ToString() + " " + _rows.ToString();
+            for (int c = 0; c < _cols; c++)
+                for (int r = 0; r < _rows; r++)
+                {
+                    iFarmX = c * _size;
+                    iFarmY = r * _size;
+                    gCanvas.FillRectangle(aBrush, iFarmX, iFarmY, _size, _size);
+                    if (_aFarm[c, r] == iPetSCat)
+                    {
+                        gCanvas.DrawString("k", aFont, Brushes.White, new Point(iFarmX, iFarmY));
+                    }
+                    if (_aFarm[c, r] == iPetSDog)
+                    {
+                        gCanvas.DrawString("d", aFont, Brushes.White, new Point(iFarmX, iFarmY));
+                    }
+                }
+        }
+
         public Form1()
         {
             InitializeComponent();
             aPen = new Pen(Color.Red, 2);
             aBrush = new SolidBrush(Color.Green);
             aFont = new Font("System", 10);
+            aFarm = InitFarm(10, 10, 9);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             MyPet = null;
             switch (new Random().Next(1, 3))
             {
@@ -37,21 +95,20 @@ namespace PetsFarm
                 case 2:
                     MyPet = new cDog("Zhuchka");
                     break;
-                /*
-                default:
-                    break;*/
+
             }
             if (MyPet != null)
             {
                 label1.Text = MyPet.doVoice();
-            }
+            }*/
+
+            aFarm = InitFarm(10, 10, 9);
+            pictureBox1.Refresh();
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            //e.Graphics.DrawRectangle(aPen, 5, 5, 20, 20);
-            e.Graphics.FillRectangle(aBrush, 5, 5, 15, 15);
-            e.Graphics.DrawString("d", aFont, Brushes.White, new Point(5, 5));
+            RenderFarm(aFarm, e.Graphics);
         }
     }
 }
